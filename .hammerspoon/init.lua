6bd1cd57992d526e7d48c526b5d5c074c55116fe
Dezer <my@ipro.dev>
2024-02-28 21:12:19 +0200

    Update from 2024-02-28T19:12:18.392011Z

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -17,7 +17,7 @@ hs.window.setShadows(false)
 
 require("plugins.caffeine")
 hs.hotkey.bind(cc, "C", function()
-  toggleCaffeine()
+	toggleCaffeine()
 end)
 
 -----------------------
@@ -25,15 +25,15 @@ end)
 -----------------------
 
 function reloadConfig(files)
-  doReload = false
-  for _, file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-    end
-  end
-  if doReload then
-    hs.reload()
-  end
+	doReload = false
+	for _, file in pairs(files) do
+		if file:sub(-4) == ".lua" then
+			doReload = true
+		end
+	end
+	if doReload then
+		hs.reload()
+	end
 end
 
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
@@ -44,9 +44,9 @@ hs.alert.show("Config loaded")
 ---------------------------
 
 function bindAppWithNameToKey(name, key)
-  hs.hotkey.bind(cc, key, function()
-    hs.application.open(name)
-  end)
+	hs.hotkey.bind(cc, key, function()
+		hs.application.open(name)
+	end)
 end
 
 hs.application.enableSpotlightForNameSearches(true)
@@ -54,139 +54,139 @@ hs.application.enableSpotlightForNameSearches(true)
 bindAppWithNameToKey("Standard Notes", "N")
 bindAppWithNameToKey("Dash", "H")
 bindAppWithNameToKey("KeePassXC", "K")
-bindAppWithNameToKey("Telegram", "G")
+bindAppWithNameToKey("GoLand", "G")
 bindAppWithNameToKey("Brave Browser", "B")
 bindAppWithNameToKey("PhpStorm", "P")
-
+bindAppWithNameToKey("Telegram", "M")
 --------------------------------------------------------------------------------
 -- Window management
 
 function almostEqualFrames(first, second)
-  local delta = 25.0
+	local delta = 25.0
 
-  return math.abs(first.x - second.x) < delta
-      and math.abs(first.y - second.y) < delta
-      and math.abs(first.w - second.w) < delta
-      and math.abs(first.h - second.h) < delta
+	return math.abs(first.x - second.x) < delta
+		and math.abs(first.y - second.y) < delta
+		and math.abs(first.w - second.w) < delta
+		and math.abs(first.h - second.h) < delta
 end
 
 -- Return screen frame available for window placement
 function currentScreenFrame()
-  return hs.screen.mainScreen():frame()
+	return hs.screen.mainScreen():frame()
 end
 
 function windowLeft()
-  local frame = currentScreenFrame()
-  frame.w = frame.w / 2
-  return frame
+	local frame = currentScreenFrame()
+	frame.w = frame.w / 2
+	return frame
 end
 
 function windowRight()
-  local frame = currentScreenFrame()
-  frame.x = frame.x + frame.w / 2
-  frame.w = frame.w / 2
-  return frame
+	local frame = currentScreenFrame()
+	frame.x = frame.x + frame.w / 2
+	frame.w = frame.w / 2
+	return frame
 end
 
 function windowTop()
-  local frame = currentScreenFrame()
-  frame.h = frame.h / 2
-  return frame
+	local frame = currentScreenFrame()
+	frame.h = frame.h / 2
+	return frame
 end
 
 function windowBottom()
-  local frame = currentScreenFrame()
-  frame.y = frame.y + frame.h / 2
-  frame.h = frame.h / 2
-  return frame
+	local frame = currentScreenFrame()
+	frame.y = frame.y + frame.h / 2
+	frame.h = frame.h / 2
+	return frame
 end
 
 hs.hotkey.bind(hyper, "F", function()
-  hs.window.focusedWindow():setFrame(currentScreenFrame())
+	hs.window.focusedWindow():setFrame(currentScreenFrame())
 end)
 
 function getFocusBackToWindow(window)
-  local other = window:otherWindowsAllScreens()
+	local other = window:otherWindowsAllScreens()
 
-  for i, win in pairs(other) do
-    if win:screen() ~= window:screen() then
-      win:focus()
-      window:focus()
-      break
-    end
-  end
+	for i, win in pairs(other) do
+		if win:screen() ~= window:screen() then
+			win:focus()
+			window:focus()
+			break
+		end
+	end
 end
 
 hs.hotkey.bind(hyper, "H", function()
-  local window = hs.window.focusedWindow()
-  local windowFrame = window:frame()
-  local frame = windowLeft()
+	local window = hs.window.focusedWindow()
+	local windowFrame = window:frame()
+	local frame = windowLeft()
 
-  if almostEqualFrames(windowFrame, frame) then
-    local screen = hs.screen.mainScreen():toWest()
+	if almostEqualFrames(windowFrame, frame) then
+		local screen = hs.screen.mainScreen():toWest()
 
-    if screen then
-      window:moveToScreen(screen)
-      getFocusBackToWindow(window)
-      return
-    end
-  elseif almostEqualFrames(windowFrame, windowTop()) then
-    frame.h = windowTop().h
-  elseif almostEqualFrames(windowFrame, windowBottom()) then
-    frame.y = windowBottom().y
-    frame.h = windowBottom().h
-  end
+		if screen then
+			window:moveToScreen(screen)
+			getFocusBackToWindow(window)
+			return
+		end
+	elseif almostEqualFrames(windowFrame, windowTop()) then
+		frame.h = windowTop().h
+	elseif almostEqualFrames(windowFrame, windowBottom()) then
+		frame.y = windowBottom().y
+		frame.h = windowBottom().h
+	end
 
-  hs.window.focusedWindow():setFrame(frame)
+	hs.window.focusedWindow():setFrame(frame)
 end)
 
 hs.hotkey.bind(hyper, "L", function()
-  local window = hs.window.focusedWindow()
-  local windowFrame = window:frame()
-  local frame = windowRight()
+	local window = hs.window.focusedWindow()
+	local windowFrame = window:frame()
+	local frame = windowRight()
 
-  if almostEqualFrames(windowFrame, frame) then
-    local screen = hs.screen.mainScreen():toEast()
+	if almostEqualFrames(windowFrame, frame) then
+		local screen = hs.screen.mainScreen():toEast()
 
-    if screen then
-      window:moveToScreen(screen)
-      getFocusBackToWindow(window)
-      return
-    end
-  elseif almostEqualFrames(windowFrame, windowTop()) then
-    frame.h = windowTop().h
-  elseif almostEqualFrames(windowFrame, windowBottom()) then
-    frame.y = windowBottom().y
-    frame.h = windowBottom().h
-  end
+		if screen then
+			window:moveToScreen(screen)
+			getFocusBackToWindow(window)
+			return
+		end
+	elseif almostEqualFrames(windowFrame, windowTop()) then
+		frame.h = windowTop().h
+	elseif almostEqualFrames(windowFrame, windowBottom()) then
+		frame.y = windowBottom().y
+		frame.h = windowBottom().h
+	end
 
-  hs.window.focusedWindow():setFrame(frame)
+	hs.window.focusedWindow():setFrame(frame)
 end)
 
 hs.hotkey.bind(hyper, "J", function()
-  local windowFrame = hs.window.focusedWindow():frame()
-  local frame = windowBottom()
+	local windowFrame = hs.window.focusedWindow():frame()
+	local frame = windowBottom()
 
-  if almostEqualFrames(windowFrame, windowLeft()) then
-    frame.w = windowLeft().w
-  elseif almostEqualFrames(windowFrame, windowRight()) then
-    frame.x = windowRight().x
-    frame.w = windowRight().w
-  end
+	if almostEqualFrames(windowFrame, windowLeft()) then
+		frame.w = windowLeft().w
+	elseif almostEqualFrames(windowFrame, windowRight()) then
+		frame.x = windowRight().x
+		frame.w = windowRight().w
+	end
 
-  hs.window.focusedWindow():setFrame(frame)
+	hs.window.focusedWindow():setFrame(frame)
 end)
 
 hs.hotkey.bind(hyper, "K", function()
-  local windowFrame = hs.window.focusedWindow():frame()
-  local frame = windowTop()
+	local windowFrame = hs.window.focusedWindow():frame()
+	local frame = windowTop()
 
-  if almostEqualFrames(windowFrame, windowLeft()) then
-    frame.w = windowLeft().w
-  elseif almostEqualFrames(windowFrame, windowRight()) then
-    frame.x = windowRight().x
-    frame.w = windowRight().w
-  end
+	if almostEqualFrames(windowFrame, windowLeft()) then
+		frame.w = windowLeft().w
+	elseif almostEqualFrames(windowFrame, windowRight()) then
+		frame.x = windowRight().x
+		frame.w = windowRight().w
+	end
 
-  hs.window.focusedWindow():setFrame(frame)
+	hs.window.focusedWindow():setFrame(frame)
 end)
