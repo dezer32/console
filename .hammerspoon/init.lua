071a7445ca5e4415eb0dc32807565e69ada60666
Dezer <my@ipro.dev>
2024-04-09 20:58:14 +0300

    Update from 2024-04-09T17:58:13.378395Z

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -192,3 +192,18 @@ hs.hotkey.bind(hyper, "K", function()
 
 	hs.window.focusedWindow():setFrame(frame)
 end)
+
+----------------------
+-- Screen managment --
+----------------------
+
+-- bind hotkey
+hs.hotkey.bind(hyper, "n", function()
+	-- get the focused window
+	local win = hs.window.focusedWindow()
+	-- get the screen where the focused window is displayed, a.k.a. current screen
+	local screen = win:screen()
+	-- compute the unitRect of the focused window relative to the current screen
+	-- and move the window to the next screen setting the same unitRect
+	win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
