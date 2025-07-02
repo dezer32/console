a56fb588c400a22f31abe59074ffe8bbe8ae580a
Dezer <my@ipro.dev>
2025-07-02 21:34:43 +0300

    Update from 2025-07-02T18:34:41.856253Z

--- .claude.mcp.sh
+++ .claude.mcp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# git clone https://github.com/minhalvp/android-mcp-server.git
+# cd android-mcp-server
+# uv python install 3.11
+# uv sync
+# claude mcp add android -- uv --directory /Users/vladislav_k/Code/mcp/android-mcp-server run server.py
+
+
 echo 'Adding MCPs to Claude...'
 
 echo 'Building context7-mcp Docker image...'
@@ -16,11 +23,25 @@ echo 'Done building context7-mcp Docker image.'
 
 claude mcp add context7             -- docker run -i --rm context7-mcp-image
 claude mcp add everything           -- docker run -i --rm mcp/everything
-claude mcp add memory               -- docker run -i --rm -v /Users/dezer/.mcp-memory/share/chroma_db:/app/chroma_db -v /Users/dezer/.mcp-memory/share/backups:/app/backups -e MCP_MEMORY_CHROMA_PATH='/app/chroma_db' -e MCP_MEMORY_BACKUPS_PATH='/app/backups' mcp-memory-service:latest
+# claude mcp add memory               -- docker run -i --rm \
+  # -v ${HOME}/.mcp-memory/share/chroma_db:/app/chroma_db \
+  # -v ${HOME}/.mcp-memory/share/backups:/app/backups \
+  # -e MCP_MEMORY_CHROMA_PATH='/app/chroma_db' \
+  # -e MCP_MEMORY_BACKUPS_PATH='/app/backups' \
+  # mcp-memory-service:latest
+claude mcp add memory               -- docker run -i --rm \
+                                        -v claude-memory:/app/dist \
+                                        -v ${HOME}/.mcp-memory/default:${HOME}/.mcp-memory/default \
+                                        -e MEMORY_FILE_PATH="${HOME}/.mcp-memory/default/memory.json" \
+                                        mcp/memory
 claude mcp add fetch                -- docker run -i --rm mcp/fetch
 claude mcp add sequential-thinking  -- docker run -i --rm mcp/sequentialthinking
 claude mcp add time                 -- docker run -i --rm mcp/time
-claude mcp add filesystem           -- docker run -i --rm --mount type=bind,src=/Users/dezer/Code/ai-invest,dst=/projects/ai-invest mcp/filesystem /projects
-claude mcp add git                  -- docker run -i --rm --mount type=bind,src=/Users/dezer/Code/ai-invest,dst=/Users/dezer/Code/ai-invest mcp/git
+claude mcp add filesystem           -- docker run -i --rm \
+  --mount type=bind,src=${HOME}/Code/,dst=${HOME}/Code \
+  mcp/filesystem ${HOME}/Code
+claude mcp add git                  -- docker run -i --rm \
+  --mount type=bind,src=${HOME}/Code/,dst=${HOME}/Code/ \
+  mcp/git
 
 
