f5a33c7f47f804f20eb552e26f50af0d1782c57c
Dezer <my@ipro.dev>
2025-08-05 09:10:14 +0300

    Update from 2025-08-05T06:10:13.608301Z

--- .claude.mcp.sh
+++ .claude.mcp.sh
@@ -9,9 +9,9 @@
 
 # echo 'Adding MCPs to Claude...'
 
-# echo 'Building context7-mcp Docker image...'
-# docker buildx build -t context7-mcp-image git@github.com:dezer32/context7.git
-# echo 'Done building context7-mcp Docker image.'
+echo 'Building context7-mcp Docker image...'
+docker buildx build -t context7-mcp-image git@github.com:dezer32/context7.git
+echo 'Done building context7-mcp Docker image.'
 #
 # echo 'Building mcp-memory-service Docker image...'
 # docker buildx build -t mcp-memory-service git@github.com:dezer32/mcp-memory-service.git
@@ -87,15 +87,20 @@ claude mcp remove --scope local sequential-thinking
 claude mcp remove --scope user fetch
 claude mcp remove --scope local fetch
 
+claude mcp remove --scope user repomix
+claude mcp remove --scope local repomix
+
 claude mcp remove --scope user claude
 claude mcp remove --scope local claude
 
 claude mcp add --scope user filesystem -- npx -y @modelcontextprotocol/server-filesystem /Users/vladislav_k/Code/ /Users/vladislav_k/Downloads/ /Users/vladislav_k/Documents/
-claude mcp add --scope user context7 -- npx -y @upstash/context7-mcp
-claude mcp add --scope user firefly-iii -- npx -y @firefly-iii-mcp/local --pat eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNWMxZDAzYmZlNGFiZGE5ZjhkYWFhODQ0MTMyOGEyZjZkYmZkOWE0MTQ1YzBlOWMyYWMwNmNiMjhkMTMwZDBkOTFhYmEzNGFmZDFlYzQwODAiLCJpYXQiOjE3NTM1Mjk1OTQuNDIwNzU2LCJuYmYiOjE3NTM1Mjk1OTQuNDIwNzU5LCJleHAiOjE3ODUwNjU1OTQuMzQ5ODkyLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.FAV2slCZnXNe6afvpwOe1GXyDFEfOskxInCYuQVOEOOVmcWJfzlyRezC5iVXuNuep0zlaRgSZz4LgUkLv12hyw4X0WYmGVdqy9nIthgBrzvYaPfNG3Vv8Ag7MsEe5U6EkspRMDAknzNEL970IaIC2-xMx5h4A7oJyDjCUYaI8BS8UFF1mE8kVWlLFmdFgORwkbQkEUMuFp9TViyh9ZxdVEV6VQtYsLxLVUER8rw3t6BIPfoHUrcn4IBq5FCNHDf0xAaC6AkfkdEYOqBd37OYm0fCkMnA-NEaUgA_xBUXxQ4WTMrfpKTjTPNU2yxg5IPD211rM9sk_P5UgoEdqw_Go_DN3rmep_-TwtZ1hUuexm7mQJD2tWtAcETR9X1UW47SNsGzriwCGsY0JOc9noJeecu3fn7CFosIrjpM9RBv_eg0kIhVVpbKF8QWMaKdlsRRP-TpvObkNf9GlHvSW5CEBax_ilEz8kbPSLePOTQdgOKOsOMsOGGWlDcqIr2s2q0ahcUCKOIAJMgaPHaX3g9wJCvWAh4dHQtDRUn--mvqrKTas0LHbeiyqS2uC9yT8R0nn9vfNUhshe-901BPGK6CCuvynVuoivgR60yXYMZzB0pEqO5mdR57dBI2eZjIjfZOLcgMz2LOcAsPL_Wr8duEUTGpGSMF2cYqC1-1e8rPI4c --baseUrl https://funds.appservice.space
+# claude mcp add --scope user context7 -- npx -y @upstash/context7-mcp
+claude mcp add --scope user context7             -- docker run -i --rm context7-mcp-image
+# claude mcp add --scope user firefly-iii -- npx -y @firefly-iii-mcp/local --pat eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNWMxZDAzYmZlNGFiZGE5ZjhkYWFhODQ0MTMyOGEyZjZkYmZkOWE0MTQ1YzBlOWMyYWMwNmNiMjhkMTMwZDBkOTFhYmEzNGFmZDFlYzQwODAiLCJpYXQiOjE3NTM1Mjk1OTQuNDIwNzU2LCJuYmYiOjE3NTM1Mjk1OTQuNDIwNzU5LCJleHAiOjE3ODUwNjU1OTQuMzQ5ODkyLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.FAV2slCZnXNe6afvpwOe1GXyDFEfOskxInCYuQVOEOOVmcWJfzlyRezC5iVXuNuep0zlaRgSZz4LgUkLv12hyw4X0WYmGVdqy9nIthgBrzvYaPfNG3Vv8Ag7MsEe5U6EkspRMDAknzNEL970IaIC2-xMx5h4A7oJyDjCUYaI8BS8UFF1mE8kVWlLFmdFgORwkbQkEUMuFp9TViyh9ZxdVEV6VQtYsLxLVUER8rw3t6BIPfoHUrcn4IBq5FCNHDf0xAaC6AkfkdEYOqBd37OYm0fCkMnA-NEaUgA_xBUXxQ4WTMrfpKTjTPNU2yxg5IPD211rM9sk_P5UgoEdqw_Go_DN3rmep_-TwtZ1hUuexm7mQJD2tWtAcETR9X1UW47SNsGzriwCGsY0JOc9noJeecu3fn7CFosIrjpM9RBv_eg0kIhVVpbKF8QWMaKdlsRRP-TpvObkNf9GlHvSW5CEBax_ilEz8kbPSLePOTQdgOKOsOMsOGGWlDcqIr2s2q0ahcUCKOIAJMgaPHaX3g9wJCvWAh4dHQtDRUn--mvqrKTas0LHbeiyqS2uC9yT8R0nn9vfNUhshe-901BPGK6CCuvynVuoivgR60yXYMZzB0pEqO5mdR57dBI2eZjIjfZOLcgMz2LOcAsPL_Wr8duEUTGpGSMF2cYqC1-1e8rPI4c --baseUrl https://funds.appservice.space
 claude mcp add --scope user git -- uvx mcp-server-git
 claude mcp add --scope user memory -e MEMORY_FILE_PATH="/Users/vladislav_k/.claude/mcp-memory/memory.json" -- npx -y @modelcontextprotocol/server-memory
 claude mcp add --scope user time -- uvx mcp-server-time
 claude mcp add --scope user sequential-thinking -- npx -y @modelcontextprotocol/server-sequential-thinking
 claude mcp add --scope user fetch -- uvx mcp-server-fetch
+claude mcp add --scope user repomix -- repomix --mcp
 claude mcp add --scope user claude -- /Users/vladislav_k/.claude/local/claude mcp serve
